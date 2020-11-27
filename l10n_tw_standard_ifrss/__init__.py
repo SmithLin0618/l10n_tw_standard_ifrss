b42072933e8d1150c81a6a0202c300e20b5cb048
SmithLin0618 <38001362+SmithLin0618@users.noreply.github.com>
2020-11-28 05:14:40 +0800

    調整為14

--- l10n_tw_standard_ifrss/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-# License LGPL-3.0
-# Copyright (C) 2017-now  元植管理顧問chingyun@yuanchih-consult.com
