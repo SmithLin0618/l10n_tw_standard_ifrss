83513f1e46d5375152e7fa520c019e1e7e7bfd9e
SmithLin0618 <38001362+SmithLin0618@users.noreply.github.com>
2023-11-03 05:48:02 +0800

    Update __manifest__.py

--- l10n_tw_standard_ifrss/__manifest__.py
+++ l10n_tw_standard_ifrss/__manifest__.py
@@ -13,29 +13,6 @@
     'images': ['static/description/twbs-1.png'],
     'description': """
 
-本模塊於14版進行功能任務調整，調整後提供以下功能：
-
-*1.台灣常用會計項目匯入(15版依經濟部商業司-商業會計項目表編制(109年版))
-
-*2.依odoo社區版財務報表模組(accounting_pdf_reports)為報表基礎呈現之台灣使用習慣財務報表結構
-
-相關社區版財務報表運作方式、元植財務報表邏輯結構與年度結轉方式請詳以下文章：
-
-https://www.yuanchih-consult.com/blog/odoo-1/post/odooodoo-15
-
-odoo社區版系統上線前之基礎知識-odoo應用雲端課程說明：
-
-https://www.yuanchih-consult.com/page/onlineclass
-
-*本模組之使用請先下載並安裝l10n_tw模組
-
-https://github.com/Odoo-Taiwan/l10n_tw
-
-報表結構呈現範例：
-<img src="general_ledger_filter.png">
-
-
-
 
 *特別感謝：
 ★技術支援：新北工具人
